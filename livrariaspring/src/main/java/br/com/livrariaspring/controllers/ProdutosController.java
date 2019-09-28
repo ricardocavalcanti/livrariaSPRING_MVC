f685a49070f5fb994ef9a65778cedc8a635f3128
ricardo <ricardo@rainduck>
2019-09-28 17:21:44 -0300

    Validacao e conversao de dados

--- livrariaspring/src/main/java/br/com/livrariaspring/controllers/ProdutosController.java
+++ livrariaspring/src/main/java/br/com/livrariaspring/controllers/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.livrariaspring.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.livrariaspring.dao.ProdutoDao;
 import br.com.livrariaspring.models.Produto;
 import br.com.livrariaspring.models.TipoPreco;
+import br.com.livrariaspring.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -19,6 +25,15 @@ public class ProdutosController {
 
 	@Autowired
 	private ProdutoDao produtoDao;
+	
+	//AMARRA O PRODUTOVALIDATION
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		
+		binder.addValidators(new ProdutoValidation());
+		
+	}
+	
 
 	@RequestMapping("/form")
 	public ModelAndView form() {
@@ -33,9 +48,17 @@ public class ProdutosController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes) {
+	public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
 
 		System.out.println(produto);
+		
+		//VALIDACAO CAMPOS PRODUTO
+		if(result.hasErrors()) {
+			
+			return form();
+			
+			
+		}
 		
 		produtoDao.gravar(produto);		
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
